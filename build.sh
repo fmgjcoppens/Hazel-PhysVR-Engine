699d2f7c91a2ba2600dd465512361cf23a554a34
Francois Coppens <francois.coppens@irsamc.ups-tlse.fr>
2023-09-23 15:24:40 +0200

    Build script

--- build.sh
+++ build.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 
+source build.vars
 ACTION=$1
 
-source build.vars
+configure () {
+    cmake \
+        -DCMAKE_CXX_COMPILER=${COMPILER} \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+        -S . -B ${BUILD_DIR}
+}
+
+build () {
+    (cd ${BUILD_DIR} && make -j)
+    [[ -f "${BUILD_DIR}/SandboxApp" ]] && ln -svf ${BUILD_DIR}/SandboxApp ${APP_NAME}
+}
+
+clean () {
+    rm -rfv ${BUILD_DIR}
+    rm -fv ${APP_NAME}
+}
 
 case $ACTION in
     configure)
-        cmake \
-            -DCMAKE_CXX_COMPILER=${COMPILER} \
-            -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-            -S . -B ${BUILD_DIR}
+        configure
         ;;
     build)
-        (cd ${BUILD_DIR} && make -j)
-        [[ -f "${BUILD_DIR}/SandboxApp" ]] && ln -svf ${BUILD_DIR}/SandboxApp ${APP_NAME}
+        build
         ;;
     clean)
-        rm -rfv ${BUILD_DIR}
-        rm -fv ${APP_NAME}
+        clean
         ;;
     *)
-        echo "Unknow or empty argument. Chosing 'build' as default!"
-        (cd ${BUILD_DIR} && make -j)
-        [[ -f "${BUILD_DIR}/SandboxApp" ]] && ln -svf ${BUILD_DIR}/SandboxApp ${APP_NAME}
+        build
         ;;
 esac
