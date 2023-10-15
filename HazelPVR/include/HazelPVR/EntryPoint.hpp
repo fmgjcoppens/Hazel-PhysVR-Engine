0e780882ef3a902dbfff0ff2b60c65ef9fbacd5f
Francois Coppens <francois.coppens@irsamc.ups-tlse.fr>
2023-10-15 08:45:04 +0200

    Loggin warning when logger is initialized.

--- HazelPVR/include/HazelPVR/EntryPoint.hpp
+++ HazelPVR/include/HazelPVR/EntryPoint.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
     HazelPVR::Log::Init();
+    HZPVR_CORE_WARN("Initialized Log!");
 
     auto app = HazelPVR::CreateApplication();
     app->Run();
